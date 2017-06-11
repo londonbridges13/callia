9f6da87603032c7a8ed131093ab4b1471f540dd3
Lyndon McKay <lyndonmckay13@gmail.com>
2017-06-10 23:01:21 -0500

    Remove migration fiion file

--- db/migrate/20170601162810_add_supervisor_to_office.rb
+++ db/migrate/20170601162810_add_supervisor_to_office.rb
@@ -1,5 +1,9 @@
 class AddSupervisorToOffice < ActiveRecord::Migration
   def change
-    add_reference :offices, :supervisor, foreign_key: { to_table: :caregivers }
+    # add_reference :offices, :supervisor, foreign_key: { to_table: :caregivers }
+    change_table :offices do |o|
+      o.references :supervisor, references: :caregiver #-> foo_bar_store_id
+    end
   end
+
 end
