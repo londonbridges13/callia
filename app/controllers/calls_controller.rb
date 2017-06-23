ae676adaca81626e18cb95f1f05be768826a4795
Lyndon McKay <lyndonmckay13@gmail.com>
2017-06-22 21:09:01 -0500

    Api

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -11,7 +11,7 @@ class CallsController < ApplicationController
   def voice
    from = params['PhoneNumber']
    response = Twilio::TwiML::Response.new do |r|
-     r.Say 'Hey there from #{from}. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
+     r.Say 'Hey there from '+ from + '. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
         r.Play 'http://linode.rabasa.com/cantina.mp3'
    end
 
