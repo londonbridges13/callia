ed6e88066b695ac95804f52544c14b9d8f312704
Lyndon McKay <lyndonmckay13@gmail.com>
2017-06-28 22:58:15 -0500

    API

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -10,7 +10,7 @@ class CallsController < ApplicationController
 
   def voice
    from = params['PhoneNumber']
-   response = Twilio::TwiML::Response.new do |r|
+  #  response = Twilio::TwiML::Response.new do |r|
      @call = Call.new
      @call.caller_number = params['From']
      @call.called_number = params['To']
@@ -18,9 +18,9 @@ class CallsController < ApplicationController
     #  r.Say "Hey there, you've called #{params['To']}. Congrats on integrating Twilio into your Rails 4 app.", :voice => 'alice'
         # r.Play 'http://linode.rabasa.com/cantina.mp3'
     ask_for_employee_code
-   end
+  #  end
 
-   render_twiml response
+  #  render_twiml response
   end
 
   def caregiver
