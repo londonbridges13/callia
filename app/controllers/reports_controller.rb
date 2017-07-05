d46c5477f400a50b32c3c986e8354d3cab37c6be
Lyndon McKay <lyndonmckay13@gmail.com>
2017-07-05 13:25:34 -0500

    API Calls

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,6 @@ class ReportsController < ApplicationController
 
   def call_logs #reports/call_logs
     # @caregivers = current_user.caregivers
-    @calls = current_user.calls
+    @calls = current_user.calls.order("created_at DESC")
   end
 end
