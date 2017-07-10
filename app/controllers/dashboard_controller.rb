6a05cbf3641ab7bddfafc25dbeec00e0c09040de
Lyndon McKay <lyndonmckay13@gmail.com>
2017-07-10 09:36:42 -0500

    API

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -56,6 +56,8 @@ class DashboardController < ApplicationController
   def graph_clock_out
     hours = [[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0],
     [13,0],[14,0],[15,0],[16,0],[17,0],[18,0],[19,0],[20,0],[21,0],[22,0],[23,0],[24,0]]
+    start = Date.today.beginning_of_day
+    the_end = Date.tomorrow.beginning_of_day
     calls = Call.where(log_type: "Clocked Out").where(user: current_user).where('created_at BETWEEN ? AND ?', start, the_end)
     calls.each do |c|
       hour = c.created_at.strftime("%H").to_i
