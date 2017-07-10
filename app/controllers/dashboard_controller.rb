ca65c63f837cf48bf82f0eff88ec211ac3876fcd
Lyndon McKay <lyndonmckay13@gmail.com>
2017-07-10 14:45:17 -0500

    API

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -74,7 +74,7 @@ class DashboardController < ApplicationController
 
     calls.each do |c|
       hour = c.to_i
-      p c.datetime
+      p c.to_time
       hours[hour - 1][1] += 1 # add 1 to call count
       p hours
     end
