c9db0b29fe5a408de96ff4d244598aad0c557c50
Lyndon McKay <lyndonmckay13@gmail.com>
2017-08-01 08:37:05 -0500

    API

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Callia
     config.active_record.raise_in_transactional_callbacks = true
 
     config.serve_static_assets = true
+    config.serve_static_files = true # same as serve_static_assets
 
   end
 end
