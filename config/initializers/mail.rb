ffa011f84dd01cbf22466b91476e203dca816de6
Lyndon McKay <lyndonmckay13@gmail.com>
2017-08-07 10:28:21 -0500

    API

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,12 @@
 require 'mail'
 
 Mail.defaults do
-  delivery_method :smtp, {
-    :port      => 587,
-    :address   => "smtp.mailgun.org",
-    :user_name => "postmaster@support.callia.us",
-    :password  => "dcd2d62a798dfa0f863bb70b9edb8f7d",
-    
-  }
+  if ENV[:mailgunpw]
+    delivery_method :smtp, {
+      :port      => 587,
+      :address   => "smtp.mailgun.org",
+      :user_name => ENV['mailgunuser'],
+      :password  => ENV['mailgunpw'],
+    }
+  end
 end
