c5add2e37ed7ea88dd4f2064bda32fa83d6a40e2
Lyndon McKay <lyndonmckay13@gmail.com>
2017-08-24 10:15:46 -0500

    M

--- app/assets/javascripts/digital_signature.js
+++ app/assets/javascripts/digital_signature.js
@@ -62,19 +62,7 @@
 //= require codemirror/mode/javascript/javascript.js
 
 
-function getLocation(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        var x = document.getElementById("demo");
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
 
-function showPosition(position) {
-    var x = document.getElementById("demo");
-    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-}
 
 function verify_location(long,lat){
 
