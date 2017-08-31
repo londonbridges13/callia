d1f0a591d4cdc7fdb11cf76e58e2d166968c40cb
Lyndon McKay <lyndonmckay13@gmail.com>
2017-08-31 18:34:28 -0500

    API

--- app/assets/javascripts/digital_signature.js
+++ app/assets/javascripts/digital_signature.js
@@ -1,3 +1,9 @@
+//= require iCheck/icheck.min.js
+//= require jeditable/jquery.jeditable.js
+//= require dataTables/datatables.min.js
+//= require jqGrid/i18n/grid.locale-el.js
+//= require jqGrid/jquery.jqGrid.min.js
+//= require footable/footable.all.min.js
 
 //= require iCheck/icheck.min.js
 //= require steps/jquery.steps.min.js
