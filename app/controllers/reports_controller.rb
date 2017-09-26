569a081ed4ece30f02e69004a902bc42e06dccde
Lyndon McKay <lyndonmckay13@gmail.com>
2017-09-26 07:30:23 -0500

    timesheet

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -252,13 +252,10 @@ class ReportsController < ApplicationController
     @organized_activity.each do |a|
       if existing_sections.include? a[0]
         i = existing_sections.index(a[0])
-        @displayable_sections[i].push [[a[1][0][0],[a[1][0][1][0],a[1][0][1][1]]]]
+        @displayable_sections[i].push [[a[1][0][0],[[a[1][0][1][0],a[1][0][1][1]]]]]
         a[1].each do |q|
           if existing_activities.include? q[0]
             ii = existing_activities.index(q[0])
-            p a[1][0][1][0][0]
-            p a[1][0][1][0][0]
-            p [[a[1][0][1][0],a[1][0][1][1]]
             @displayable_sections[i][1][1].push a[1][0][1][0][0]
           end
         end
